490f44e21ecdec5b3c3f0865954bea8ba14f71b4
Sweetpurple616 <zoedewaard@hotmail.nl>
2018-05-23 16:43:12 +0200

    mijn code toegevoegd

--- dev/Items/Banaan.ts
+++ dev/Items/Banaan.ts
@@ -1,17 +1,19 @@
 
 
+
 class Banaan {
-    banaan:object;
+    
     constructor(){
         this.create()
 
     }
     create(){
-        //aod.playground.
-        let banaan = this.physics.add.staticGroup();
-        banaan.create(100, 400, 'banaan').setScale(0.3)
-        banaan.create(, 50, 'banaan').setScale(0.3)
+        
+        let banaan = aod.playground.physics.add.staticGroup();
+        banaan.create(250, 650, 'banaan').setScale(0.3)
+        banaan.create(450, 50, 'banaan').setScale(0.3)
         banaan.create(100, 100, 'banaan').setScale(0.3)
     }
 
+}
 }
--- dev/Levels/level1.ts
+++ dev/Levels/level1.ts
@@ -1,12 +1,13 @@
 class Level1 {
     constructor() {
         aod.playground.add.image(window.innerWidth / 2, 300, 'background').setScale(1.2);
-        let platforms = aod.playground.physics.add.staticGroup();
-        platforms.create(0, 50, 'ground').setScale(0.3).refreshBody();
-        platforms.create(700, 230, 'ground').setScale(0.3).refreshBody();
-        platforms.create(1400, 150, 'ground').setScale(0.3).refreshBody();
 
+        let platforms = this.physics.add.staticGroup();
+        platforms.create(100, 700, 'ground').setScale(0.3).refreshBody();
+        platforms.create(700, 500, 'ground').setScale(0.3).refreshBody();
+        platforms.create(1200, 700, 'ground').setScale(0.3).refreshBody();
 
+        
         aod.player = new Player();
         aod.aphrodite = new Aphrodite();
         aod.banaan = new Banaan();
@@ -18,6 +19,6 @@ class Level1 {
         aod.playground.physics.world.setBounds(0, 0, 1260 * 4, 720 * 2);
     }
     update() {
-
+        aod.player.movement()
     }
 }
