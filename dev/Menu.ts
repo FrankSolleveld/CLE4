79265ef2d915839e54b2e6f7143d215fc48031c8
IAmMisterFrank <fsolleveld@outlook.com>
2018-05-30 10:28:39 +0200

    Added enter to start game code in menu.ts

--- dev/Menu.ts
+++ dev/Menu.ts
@@ -1,30 +1,33 @@
-class Menu 
-{
-    constructor() 
-    {
+class Menu {
+    constructor() {
 
         this.init()
         this.create()
+        this.clickable()
 
     }
 
-    init() 
-    {
+    init() {
         aod.playground.add.image(window.innerWidth / 2, window.innerHeight, 'retro-game-bg').setScale(2);
-        
+
         aod.playground.titleText = aod.playground.add.text(0, 0, "Ape of Divinity", {
-            boundsAlignH:'center'
+            boundsAlignH: 'center'
         });
 
+
+        // FONT WORDT LATER AANGEPAST
         // aod.playground.titleText = aod.playground.add.bitmapText(aod.playground, 50, 50, 'arcadeclassic', 'Ape of Divinity', 50, 'center'); 
         // aod.playground.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
         // aod.playground.titleText.anchor.set(0.5);
 
-        aod.playground.optionCount = 1;
+        // aod.playground.optionCount = 1;
     }
 
-    create() 
-    {
+    clickable() {
+        let Ekey = aod.playground.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+    }
+
+    create() {
 
         // if (aod.music.name !== "dangerous" && playMusic) {
         //     aod.music.stop();
@@ -49,29 +52,17 @@ class Menu
         //     game.state.start("Options");
         // });
 
+        if (Ekey.isDown) {
+            // Zodra er op Enter is gedrukt, wordt je naar level 1 gebracht.
+            this.scene = new Level1()
+        }
+
     }
-    // var GameMenu = function () { }
-
-    // GameMenu.prototype = {
-
-    //     menuConfig: {
-    //         startY: 260,
-    //         startX: 30
-    //     },
-
-    //     init: function () {
-    //         
-    //     },
-
-    //     create: function () {
 
 
-    //     }
-    // };
-
-    // Phaser.Utils.mixinPrototype(GameMenu.prototype, mixins);
-
-
+    update() {
+        this.scene.update()
+    }
 
 
 }
