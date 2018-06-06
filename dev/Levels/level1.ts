005b475c131f415d89bef79102edc22e56f89aeb
IAmMisterFrank <fsolleveld@outlook.com>
2018-06-06 12:02:54 +0200

    - Added story to level 1

--- dev/Levels/level1.ts
+++ dev/Levels/level1.ts
@@ -1,18 +1,18 @@
 class Level1 {
 
-    startDialog:Dialog;
-    aphrodite:Aphrodite;
-    talkingToAphrodite:boolean = false;
-    part:number = 0;
+    startDialog: Dialog;
+    aphrodite: Aphrodite;
+    talkingToAphrodite: boolean = false;
+    part: number = 0;
 
     constructor() {
-        
+
         // Add background
         aod.playground.add.image(1200, 300, 'background').setScale(0.3);
-        
+
         // Randomize clouds
         new Clouds();
-        
+
         // Add trees to the game (behind the platforms!)
         new Tree(50, 495);
         new Tree(300, 495);
@@ -38,38 +38,38 @@ class Level1 {
         aod.playground.physics.add.overlap(aod.aphrodite.aphrodite, aod.player.player, this.hey, null, this);
         aod.playground.physics.world.setBounds(0, 0, 1260 * 4, 720 * 2);
 
-        this.startDialog = new Dialog("Zie je daar Aphrodite? Loop naar haar toe en\n\nzeg haar gedag!");
-        
+        this.startDialog = new Dialog("Een mystiek wezen staat\nin de verte op je te wachten.");
+
         aod.playground.cameras.main.setBounds(0, 0, 1280 * 2, 720);
 
         aod.playground.cameras.main.startFollow(aod.player.player, true, 0.05, 0.05);
         //aod.playground.cameras.main.
         //aod.playground.cameras.main.setBounds(0, 0, 3000, 1260);
-        
+
     }
 
     update() {
-        aod.player.movement()  
-        if (aod.player.player.x >= 1280 && this.part == 0){
+        aod.player.movement()
+        if (aod.player.player.x >= 1280 && this.part == 0) {
             // aod.player.player.x = 125;
             // aod.player.player.y = 200;
             this.level1Part2()
 
-    }
-        if (aod.player.player.y >= 800){
+        }
+        if (aod.player.player.y >= 800) {
             aod.player.player.x = 125;
-            aod.player.player.y = 200;   
+            aod.player.player.y = 200;
         }
     }
 
     hey() {
         // Already talking to Aphrodite?
         // YES: Do not trigger it again
-        if(this.talkingToAphrodite) { return; }
+        if (this.talkingToAphrodite) { return; }
 
         // Update the object, saying we're talking to Aphrodite
         this.talkingToAphrodite = true;
-        
+
         // Hide start dialog
         this.startDialog.hide();
 
@@ -77,29 +77,33 @@ class Level1 {
         //aod.player.disableWalking();
 
         // Show talking dialog
-        let aphrodite_dialog_1 = new Dialog("Aphrodite:\n\nHoi ..., wat zie jij er leuk uit!", () => {
-            aphrodite_dialog_1.hide();
+        let ape_dialog_1 = new Dialog("Aapje:\n\nWow, wie ben jij?", () => {
+            ape_dialog_1.hide();
 
-            let ape_dialog_1 = new Dialog("Aapje:\n\nJa hoi! Dit is een stukje storyline!", () => {
-                ape_dialog_1.hide();
+            let aphrodite_dialog_1 = new Dialog("Aphrodite:\n\nIk ben Aphrodite, Griekse godin van de\nverleiding. Kijk uit of je zal mij ook niet\nkunnen weerstaan. ", () => {
+                aphrodite_dialog_1.hide();
 
-                let aphrodite_dialog_2 = new Dialog("Aphrodite:\n\nJa dat klopt, leuke invulling!", () => {
-                    aphrodite_dialog_2.hide();
+                let ape_dialog_2 = new Dialog("Ape:\n\nDat zal moeilijk worden met al\ndie bananen om je heen.", () => {
+                    ape_dialog_2.hide();
 
-                    let ape_dialog_2 = new Dialog("Aapje:\n\nOke ik zal de bananen sparen!", () => {
-                        ape_dialog_2.hide();
-                        
-                        // Let the player walk again
-                        aod.player.enableWalking();
-                        
+                    let aphrodite_dialog_2 = new Dialog("Aphrodite:\n\nAls jij mij echt kan weerstaan zal je dat\nmoeten bewijzen door al de bananen te\nverzamelen.", () => {
+                        aphrodite_dialog_2.hide();
+
+                        let ape_dialog_3 = new Dialog("Ape:\n\nKlinkt als een uitdaging.", () => {
+                            ape_dialog_3.hide();
+
+                            // Let the player walk again
+                            aod.player.enableWalking();
+
+                        });
                     });
                 });
-            });    
+            });
         });
-        
+
 
     }
-    level1Part2(){
+    level1Part2() {
         aod.score = new Score();
         this.part = 2;
         aod.aphrodite.delete();
